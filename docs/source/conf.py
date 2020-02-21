459e780b4449c7b3ae94007fcdd94589d94502b3
Todd Sherwood <sherwood.todd@epa.gov>
2020-02-21 16:57:34 -0500

    Bump version: 0.11.1 → 0.11.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, OTAQ ASD'
 author = 'OTAQ ASD'
 
 # The full version, including alpha/beta/rc tags
-release = "0.11.1"
+release = "0.11.2"
 
 # -- General configuration ---------------------------------------------------
 
--- project_code/__init__.py
+++ project_code/__init__.py
@@ -1,3 +1,3 @@
 # Version number of usepa-cti-bca project
 
-__version__ = "0.11.1"
+__version__ = "0.11.2"
