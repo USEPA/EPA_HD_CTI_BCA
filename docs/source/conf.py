8398d614d2449d29de77b345ebc4e467252ab18e
Todd Sherwood <sherwood.todd@epa.gov>
2020-03-15 18:25:22 -0400

    Bump version: 0.17.1 → 0.17.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, OTAQ ASD'
 author = 'OTAQ ASD'
 
 # The full version, including alpha/beta/rc tags
-release = "0.17.1"
+release = "0.17.2"
 
 # -- General configuration ---------------------------------------------------
 
