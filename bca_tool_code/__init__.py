e783f93cd3e1c2a2506bbbca13526f0ba3105b13
toddsherwood-EPA <sherwood.todd@epa.gov>
2021-11-19 18:12:16 -0500

    Bump version: 0.36.0 → 0.36.1

--- bca_tool_code/__init__.py
+++ bca_tool_code/__init__.py
@@ -1,3 +1,3 @@
 # Version number of usepa-cti-bca tool
 
-__version__ = "0.36.0"
+__version__ = "0.36.1"
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, OTAQ ASD'
 author = 'OTAQ ASD'
 
 # The full version, including alpha/beta/rc tags
-release = "0.36.0"
+release = "0.36.1"
 
 # -- General configuration ---------------------------------------------------
 
